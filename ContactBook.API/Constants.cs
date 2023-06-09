6171befdd20f3b6f949c93fd263a6e312cfaad4e
Taras-Kopanskyi <tkopanskyi@reenbit.com>
2023-06-09 22:49:37 +0300

    Remove unnecessary references

--- ContactBook.API/Constants.cs
+++ ContactBook.API/Constants.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace ContactBook.API
+﻿namespace ContactBook.API
 {
     public static class Constants
     {
